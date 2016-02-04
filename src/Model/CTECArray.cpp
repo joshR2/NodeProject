d74cd76922fae8acfa41babe1ddb6b4866d30391
joshR2 <josh2cool365@aol.com>
2016-02-04 09:55:04 -0700

    Created the deconstructor

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -35,12 +35,25 @@ CTECArray<Type> :: CTECArray(int size)
 			head = &first;
 		}
 	}
+	delete head;
 }
 
 template <class Type>
 CTECArray<Type> :: ~CTECArray()
 {
+	ArrayNode<Type> * deleteMe = head;
+	for(int index = 0; index  size; index++)
+	{
+		if(deleteMe->getNext() != nullptr)
+		{
+			head = deleteMe->getNext();
+			deleteMe->setNext(nullptr);
 
+		}
+			delete deleteMe->getNext();
+			deleteMe = head;
+	}
+	delete head;
 }
 
 template <class Type>
